d0bf51deea0468573a1e333f71a53e22ee5a9979
TT044106 <Trevin.Teacutter@Cerner.com>
2017-01-09 09:44:39 -0600

    Reverting intellij gui designer support

--- mwo-mechbay-browser/src/main/java/com/github/majora_incarnate/mwo/mechbay/browser/AboutDialogBox.java
+++ mwo-mechbay-browser/src/main/java/com/github/majora_incarnate/mwo/mechbay/browser/AboutDialogBox.java
@@ -1,33 +1,24 @@
 package com.github.majora_incarnate.mwo.mechbay.browser;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class AboutDialogBox extends JDialog {
     private static final long serialVersionUID = -6163429021390520229L;
 
-    private JPanel contentPane;
     private JButton closeButton;
-    private JLabel mwoVersionTitle;
     private JLabel mwoVersionLabel;
     private JLabel authorLabel;
-    private JLabel authorTitle;
     private JLabel disclaimerLabel;
     private JLabel emailLabel;
-    private JLabel emailTitle;
     private JLabel insigniaLabel;
     private JLabel mechbayVersionLabel;
-    private JLabel mechbayVersionTitle;
     private JPanel aboutPanel;
     private JLabel titleLabel;
 
     public AboutDialogBox(final String mechbayVersion, final String mwoVersion) {
-        setContentPane(contentPane);
         setModal(true);
-        getRootPane().setDefaultButton(closeButton);
-
         setTitle("About MWO Mechbay");
+
         mechbayVersionLabel.setText(mechbayVersion);
         mwoVersionLabel.setText(mwoVersion);
 
