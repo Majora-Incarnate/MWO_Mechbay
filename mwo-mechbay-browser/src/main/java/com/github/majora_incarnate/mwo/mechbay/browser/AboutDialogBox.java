df6a7059279caacf7822dd82aae0eeca00d31532
TT044106 <Trevin.Teacutter@Cerner.com>
2017-01-12 13:34:45 -0600

    Fixing things with the AboutDialogBox

--- mwo-mechbay-browser/src/main/java/com/github/majora_incarnate/mwo/mechbay/browser/AboutDialogBox.java
+++ mwo-mechbay-browser/src/main/java/com/github/majora_incarnate/mwo/mechbay/browser/AboutDialogBox.java
@@ -5,17 +5,26 @@
  */
 package com.github.majora_incarnate.mwo.mechbay.browser;
 
-import com.github.majora_incarnate.mwo.mechbay.entities.Database;
-import com.github.majora_incarnate.mwo.mechbay.entities.Preferences;
+import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.Point;
 import java.util.ResourceBundle;
+
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.SwingConstants;
+import javax.swing.WindowConstants;
+import javax.swing.border.TitledBorder;
+
+import com.github.majora_incarnate.mwo.mechbay.entities.Constants;
+import com.github.majora_incarnate.mwo.mechbay.entities.Database;
+import com.github.majora_incarnate.mwo.mechbay.entities.Preferences;
 
 /**
  *
@@ -55,86 +64,130 @@ public class AboutDialogBox extends JDialog {
         disclaimerLabel = new JLabel();
         closeButton = new JButton();
 
+        setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("About MWO Mechbay");
+        setLocation(new Point(0, 0));
+        setMaximumSize(new Dimension(540, 320));
+        setMinimumSize(new Dimension(540, 320));
         setName("Form"); // NOI18N
+        setPreferredSize(new Dimension(540, 320));
+        setSize(new Dimension(540, 320));
         getContentPane().setLayout(new GridBagLayout());
 
+        imageLabel.setHorizontalAlignment(SwingConstants.CENTER);
         imageLabel.setIcon(new ImageIcon(getClass().getResource("/Steel Jaguar.gif"))); // NOI18N
         imageLabel.setHorizontalTextPosition(SwingConstants.CENTER);
         imageLabel.setName("imageLabel"); // NOI18N
+        imageLabel.setPreferredSize(new Dimension(250, 250));
         gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 0;
-        gridBagConstraints.gridwidth = 2;
         gridBagConstraints.gridheight = 4;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
         getContentPane().add(imageLabel, gridBagConstraints);
 
+        mwoVersionLabel.setText(Constants.MECHBAY_VERSION);
+        mechbayVersionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        mechbayVersionLabel.setBorder(BorderFactory.createTitledBorder(null, "Mechbay Version", TitledBorder.LEADING, TitledBorder.TOP));
         mechbayVersionLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+        mechbayVersionLabel.setMinimumSize(new Dimension(250, 50));
         mechbayVersionLabel.setName("mechbayVersionLabel"); // NOI18N
+        mechbayVersionLabel.setPreferredSize(new Dimension(250, 50));
         gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 0;
         gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new Insets(5, 5, 5, 5);
         getContentPane().add(mechbayVersionLabel, gridBagConstraints);
 
         mwoVersionLabel.setText(MASTER_DATABASE.MISCELLANEOUS.getOrDefault("mwoVersion", "Unknown"));
+        mwoVersionLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        mwoVersionLabel.setBorder(BorderFactory.createTitledBorder(null, "MWO Version", TitledBorder.LEADING, TitledBorder.TOP));
         mwoVersionLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+        mwoVersionLabel.setMinimumSize(new Dimension(250, 50));
         mwoVersionLabel.setName("mwoVersionLabel"); // NOI18N
+        mwoVersionLabel.setPreferredSize(new Dimension(250, 50));
         gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 1;
         gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new Insets(5, 5, 5, 5);
         getContentPane().add(mwoVersionLabel, gridBagConstraints);
 
         authorLabel.setText(MASTER_DATABASE.MISCELLANEOUS.getOrDefault("author", "Unknown"));
+        authorLabel.setHorizontalAlignment(SwingConstants.CENTER);
         authorLabel.setText("Quicksilver Kalasa");
+        authorLabel.setBorder(BorderFactory.createTitledBorder(null, "Author", TitledBorder.LEADING, TitledBorder.TOP));
         authorLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+        authorLabel.setMinimumSize(new Dimension(250, 50));
         authorLabel.setName("authorLabel"); // NOI18N
+        authorLabel.setPreferredSize(new Dimension(250, 50));
         gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 2;
         gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new Insets(5, 5, 5, 5);
         getContentPane().add(authorLabel, gridBagConstraints);
 
         emailLabel.setText(MASTER_DATABASE.MISCELLANEOUS.getOrDefault("email", "Unknown"));
+        emailLabel.setHorizontalAlignment(SwingConstants.CENTER);
         emailLabel.setText("tjteacutter1@cougars.ccis.edu");
+        emailLabel.setBorder(BorderFactory.createTitledBorder(null, "Email", TitledBorder.LEADING, TitledBorder.TOP));
         emailLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+        emailLabel.setMinimumSize(new Dimension(250, 50));
         emailLabel.setName("emailLabel"); // NOI18N
+        emailLabel.setPreferredSize(new Dimension(250, 50));
         gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 2;
+        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 3;
         gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new Insets(5, 5, 5, 5);
         getContentPane().add(emailLabel, gridBagConstraints);
 
+        disclaimerLabel.setHorizontalAlignment(SwingConstants.CENTER);
         disclaimerLabel.setText("This is not an official MWO tool.");
+        disclaimerLabel.setBorder(BorderFactory.createTitledBorder(null, "Disclaimer", TitledBorder.LEADING, TitledBorder.TOP));
         disclaimerLabel.setHorizontalTextPosition(SwingConstants.CENTER);
+        disclaimerLabel.setMaximumSize(new Dimension(375, 50));
+        disclaimerLabel.setMinimumSize(new Dimension(375, 50));
         disclaimerLabel.setName("disclaimerLabel"); // NOI18N
+        disclaimerLabel.setPreferredSize(new Dimension(375, 50));
         gridBagConstraints = new GridBagConstraints();
         gridBagConstraints.gridx = 0;
         gridBagConstraints.gridy = 4;
-        gridBagConstraints.gridwidth = 3;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new Insets(5, 5, 5, 5);
         getContentPane().add(disclaimerLabel, gridBagConstraints);
 
         closeButton.setText("Close");
         closeButton.setHorizontalTextPosition(SwingConstants.CENTER);
+        closeButton.setMinimumSize(new Dimension(125, 50));
         closeButton.setName("closeButton"); // NOI18N
+        closeButton.setPreferredSize(new Dimension(125, 50));
         gridBagConstraints = new GridBagConstraints();
-        gridBagConstraints.gridx = 3;
+        gridBagConstraints.gridx = 1;
         gridBagConstraints.gridy = 4;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = GridBagConstraints.BOTH;
         gridBagConstraints.weightx = 1.0;
         gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new Insets(5, 5, 5, 5);
         getContentPane().add(closeButton, gridBagConstraints);
 
         pack();
